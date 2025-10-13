ed72f9e7dfbc4a0aeee189d4aef1b5ca9d683487
ttn-piw <ttrungnguyen2003@gmail.com>
2025-10-13 15:50:27 +0700

    Remove _class when saving into database

---
+++ user_service/src/main/java/com/thesis/user_service/configuration/MongoConfig.java
@@ -0,0 +1,20 @@
+package com.thesis.user_service.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+
+@Configuration
+public class MongoConfig {
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory factory,
+                                       MappingMongoConverter converter) {
+
+        //Remove _class when saving into database
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return new MongoTemplate(factory, converter);
+    }
+}
