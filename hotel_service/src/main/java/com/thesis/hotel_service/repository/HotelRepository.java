2424cfb86b2c5b3d4a7a04eef7b28311a2837060
ttn-piw <ttrungnguyen2003@gmail.com>
2025-10-20 18:01:24 +0700

    Advantage JPA with Specification

--- hotel_service/src/main/java/com/thesis/hotel_service/repository/HotelRepository.java
+++ hotel_service/src/main/java/com/thesis/hotel_service/repository/HotelRepository.java
@@ -2,10 +2,11 @@ package com.thesis.hotel_service.repository;
 
 import com.thesis.hotel_service.model.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface HotelRepository extends JpaRepository<Hotel, UUID> {
+public interface HotelRepository extends JpaRepository<Hotel, UUID>, JpaSpecificationExecutor<Hotel> {
     Hotel findHotelById(UUID id);
 
     Hotel findHotelByEmail(String email);
