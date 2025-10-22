fb99efd8f5a0100a5775096c328846e555d04c06
ttn-piw <ttrungnguyen2003@gmail.com>
2025-10-23 02:22:49 +0700

    Mapping from entity to dto

--- hotel_service/src/main/java/com/thesis/hotel_service/mapper/HotelMapper.java
+++ hotel_service/src/main/java/com/thesis/hotel_service/mapper/HotelMapper.java
@@ -2,6 +2,7 @@ package com.thesis.hotel_service.mapper;
 
 import com.thesis.hotel_service.dto.request.HotelUpdateRequest;
 import com.thesis.hotel_service.dto.request.NewHotelRequest;
+import com.thesis.hotel_service.dto.response.HotelResponse;
 import com.thesis.hotel_service.model.Hotel;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
@@ -12,6 +13,8 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 public interface HotelMapper {
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updatedHotel(@MappingTarget Hotel target, HotelUpdateRequest updatedHotel);
+
+    HotelResponse toHotelResponse(Hotel hotel);
 }
 
 
