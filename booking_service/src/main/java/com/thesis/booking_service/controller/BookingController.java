54c439bf536387dbedfe4509e79d919e81378d3f
ttn-piw <ttrungnguyen2003@gmail.com>
2025-10-24 00:37:56 +0700

    Define booking_service for system

--- booking_service/src/main/java/com/thesis/booking_service/controller/BookingController.java
+++ booking_service/src/main/java/com/thesis/booking_service/controller/BookingController.java
@@ -1,4 +1,15 @@
 package com.thesis.booking_service.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/bookings")
 public class BookingController {
+    @GetMapping("/test")
+    public String testAPI(){
+        return "Booking_service API";
+    }
 }
