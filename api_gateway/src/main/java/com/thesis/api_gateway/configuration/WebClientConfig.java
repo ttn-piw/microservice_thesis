3507d762e6c8061e41d6f97ff123878d7c587102
ttn-piw <ttrungnguyen2003@gmail.com>
2025-10-28 22:37:09 +0700

    Config for preventing block CORS with front-end

--- api_gateway/src/main/java/com/thesis/api_gateway/configuration/WebClientConfig.java
+++ api_gateway/src/main/java/com/thesis/api_gateway/configuration/WebClientConfig.java
@@ -3,9 +3,14 @@ package com.thesis.api_gateway.configuration;
 import com.thesis.api_gateway.repository.AuthClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
 
 @Configuration
 public class WebClientConfig {
@@ -14,6 +19,20 @@ public class WebClientConfig {
         return WebClient.builder()
                 .baseUrl("http://localhost:8081/api/v1/auth")
                 .build();
+    }
+
+    //Config CORS
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("*"));
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
     }
 
     @Bean
